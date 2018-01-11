5aae2c2a7c0076586688318cc4832afd9ab32e31
Pawa13 <reznik13pa@gmail.com>
2018-01-11 16:17:51 +0200

    4

--- controllers/Front_controller.php
+++ controllers/Front_controller.php
@@ -1,17 +1,17 @@
 <?php
 
-$a = filter_input(INPUT_GET, 'r');
+$a = filter_input(INPUT_GET, 'r'); //проверяем, нет ли GET параметра r 
 
-if (!$a)
+if (!$a) //если GET параметра r, нет - следовательно подключаем Shablon1.php
 {
    include 'view/Shablon1.php';
 }
-else
+else //если GET параметра r, есть - подключаем Shablon2.php
 {
     include 'view/Shablon2.php';
 }
 
-
+//таким образом образовано даже некое подобие роутинга
 
 
 
--- model/All_text_one_articl.php
+++ model/All_text_one_articl.php
@@ -1,6 +1,6 @@
  <?php
 $a = $_GET["r"];
-require_once '/var/www/fishing_blog_procedure/components/Db_connect.php';
+require_once 'components/Db_connect.php';
 $query = "SELECT ID,Date,Articl_name,Text FROM `Aticls` Where `ID` = $a";
 $result = mysqli_query($link, $query); 
 while ($row = mysqli_fetch_assoc($result))
